f2f579306548a328fc63020517080b8967376855
Md. Touhidur Rahman <bsse1434@iit.du.ac.bd>
2025-07-15 10:44:27 +0600

    Delete environment directory

--- environment/cell.py
+++
@@ -1,7 +0,0 @@
-class Cell:
-    def __init__(self, x, y, content='-'):
-        self.x = x
-        self.y = y
-        self.content = content  # '-', 'P', 'W', 'G'
-        self.visited = False
-        self.safe = False
